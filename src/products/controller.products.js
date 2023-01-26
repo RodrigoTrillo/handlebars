e3f1e84ebeb0c48a9b8d0c46221a53e44c531b11
Rodrigo Trillo <rollyt1232@gmail.com>
2023-01-26 19:53:08 -0300

    solutions in products

--- src/products/controller.products.js
+++ src/products/controller.products.js
@@ -4,19 +4,21 @@ const router= Router()
 
 const products = [
     {
-        title: "prod1",
+        title: "Zapatilla 1",
+        img:"https://grid0.vtexassets.com/arquivos/ids/706612-500-auto?v=1761172031&width=500&height=auto&aspect=true",
         stock: 121,
         description: "",
         price: 100,
         thumbnail: "reultrasecretofads",
         code: "123abdefghy",
-        category:"Product",
+        category:"Nike",
         status:true,
         id: 1
       },
     {
         title: "prod2",
         stock: 121,
+        img:"https://essential.vtexassets.com/arquivos/ids/694252-1200-auto?v=638094994137900000&width=1200&height=auto&aspect=true",
         description: "",
         price: 100,
         thumbnail: "reultrasecretofads",
@@ -28,6 +30,7 @@ const products = [
     {
         title: "prod3",
         stock: 121,
+        img:"https://grid0.vtexassets.com/arquivos/ids/705479-500-auto?v=1761155791&width=500&height=auto&aspect=true",
         description: "",
         price: 100,
         thumbnail: "reultrasecretofads",
@@ -39,6 +42,7 @@ const products = [
     {
         title: "prod4",
         stock: 121,
+        img:"https://grid0.vtexassets.com/arquivos/ids/685144-500-auto?v=1761155085&width=500&height=auto&aspect=true",
         description: "",
         price: 100,
         thumbnail: "reultrasecretofads",
@@ -51,10 +55,10 @@ const products = [
 
 router.get('/',(req, res)=>{
     res.render('products.handlebars',{
-        product: products,
+        products:products[0],
         style:'index.css'
     })
-})
+}) 
 
 
 module.exports =router
