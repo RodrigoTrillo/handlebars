00a179ab9dd08fba205f81f78961afcb5ee5676b
Rodrigo Trillo <rollyt1232@gmail.com>
2023-01-26 21:06:27 -0300

    delete foods and users

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
-const users = require('../users/controller.users')
-const foods = require('../foods/controller.foods')
 const products = require('../products/controller.products')
 
 
 const router = (app)=>{
-    app.use('/users', users)
+   
     app.use('/products', products )
-    app.use('/foods', foods)
+   
     
 }
 
